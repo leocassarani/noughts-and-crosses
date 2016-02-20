8ac4960256b3bdebd777882256816338cce5bf67
Leo Cassarani <me@leocassarani.net>
2016-02-20 15:50:50 +0000

    Tidy up main and allow the user to specify next shape

--- app/Main.hs
+++ app/Main.hs
@@ -5,19 +5,19 @@ import System.Environment (getArgs)
 import System.Exit (exitFailure)
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    (str:_) -> playGame (makeBoard str) Nought
-    []      -> putStrLn "not enough arguments" >> exitFailure
+main = getArgs >>= run
+
+run :: [String] -> IO ()
+run (board:shape:_) = playGame (makeBoard board) (read shape)
+run (board:_)       = playGame (makeBoard board) Nought -- Default to Nought
+run []              = putStrLn "Error: not enough arguments" >> exitFailure
 
 playGame :: Board -> Shape -> IO ()
 playGame board shape
   | gameOver board = return ()
-  | otherwise = do
-      let next = nextMove board shape
-      print next
-      playGame next (opponent shape)
+  | otherwise =
+    let next = nextMove board shape
+    in  print next >> playGame next (opponent shape)
 
 gameOver :: Board -> Bool
 gameOver brd = isWinFor brd Nought || isWinFor brd Cross || isDraw brd
