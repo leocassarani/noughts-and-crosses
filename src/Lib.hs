b9d11582050e567417e51fe6e83930debb4bae0e
Leo Cassarani <me@leocassarani.net>
2016-02-20 12:12:19 +0000

    Rename "Player" to "Shape"

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,10 +4,10 @@ import Data.Function (on)
 import Data.List (findIndices, minimumBy, transpose)
 import Data.Maybe (isJust, isNothing)
 
-data Player = Nought | Cross
+data Shape = Nought | Cross
   deriving (Read, Show, Eq)
 
-type Cell = Maybe Player
+type Cell = Maybe Shape
 type Board = [Cell]
 
 boardSize = 3
@@ -19,19 +19,19 @@ makeBoard = map charToCell . take (boardSize * boardSize)
     charToCell 'x' = Just Cross
     charToCell _   = Nothing
 
-nextMove :: Board -> Player -> Board
-nextMove brd pl = minimumBy (compare `on` opponentScore) (nextBoards brd pl)
+nextMove :: Board -> Shape -> Board
+nextMove brd shp = minimumBy (compare `on` opponentScore) (nextBoards brd shp)
   where
-    opponentScore brd' = scoreFor brd' (opponent pl)
+    opponentScore brd' = scoreFor brd' (opponent shp)
 
-isWinFor :: Board -> Player -> Bool
-isWinFor brd pl = any winningSlice allSlices
+isWinFor :: Board -> Shape -> Bool
+isWinFor brd shp = any winningSlice allSlices
   where
-    winningSlice = all (== Just pl)
+    winningSlice = all (== Just shp)
     allSlices = rows brd ++ cols brd ++ diagonals brd
 
-isLossFor :: Board -> Player -> Bool
-isLossFor brd pl = isWinFor brd (opponent pl)
+isLossFor :: Board -> Shape -> Bool
+isLossFor brd shp = isWinFor brd (opponent shp)
 
 isDraw :: Board -> Bool
 isDraw brd = isFull brd && noWin Nought && noWin Cross
@@ -41,10 +41,10 @@ isDraw brd = isFull brd && noWin Nought && noWin Cross
 isFull :: Board -> Bool
 isFull = all isJust
 
-nextBoards :: Board -> Player -> [Board]
-nextBoards brd pl = map makeMove emptyIdxs
+nextBoards :: Board -> Shape -> [Board]
+nextBoards brd shp = map makeMove emptyIdxs
   where
-    makeMove n = fillCell brd n (Just pl)
+    makeMove n = fillCell brd n (Just shp)
     emptyIdxs  = findIndices isNothing brd
 
 fillCell :: Board -> Int -> Cell -> Board
@@ -54,14 +54,14 @@ fillCell brd n cell
   where
     (before, after) = splitAt n brd
 
-scoreFor :: Board -> Player -> Int
-scoreFor brd pl
-  | isWinFor  brd pl = 1
-  | isLossFor brd pl = -1
-  | isDraw    brd    = 0
-  | otherwise        = -(minimum $ map opponentScore (nextBoards brd pl))
+scoreFor :: Board -> Shape -> Int
+scoreFor brd shp
+  | isWinFor  brd shp = 1
+  | isLossFor brd shp = -1
+  | isDraw    brd     = 0
+  | otherwise         = -(minimum $ map opponentScore (nextBoards brd shp))
   where
-    opponentScore brd' = scoreFor brd' (opponent pl)
+    opponentScore brd' = scoreFor brd' (opponent shp)
 
 rows :: Board -> [[Cell]]
 rows brd = map row [0..boardSize-1]
@@ -78,6 +78,6 @@ diagonals brd = map extract [topLeft, topRight]
     topLeft  = map (\n -> n * (boardSize + 1))       [0..boardSize-1]
     topRight = map (\n -> (n + 1) * (boardSize - 1)) [0..boardSize-1]
 
-opponent :: Player -> Player
+opponent :: Shape -> Shape
 opponent Nought = Cross
 opponent Cross  = Nought
