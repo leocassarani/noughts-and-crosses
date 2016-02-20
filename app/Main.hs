4187cebebcf20a9eb37990e052e59d36e75f2129
Leo Cassarani <me@leocassarani.net>
2016-02-20 15:46:52 +0000

    Add a simple main function that plays through a given board

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
 import Lib
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+  args <- getArgs
+  case args of
+    (str:_) -> playGame (makeBoard str) Nought
+    []      -> putStrLn "not enough arguments" >> exitFailure
+
+playGame :: Board -> Shape -> IO ()
+playGame board shape
+  | gameOver board = return ()
+  | otherwise = do
+      let next = nextMove board shape
+      print next
+      playGame next (opponent shape)
+
+gameOver :: Board -> Bool
+gameOver brd = isWinFor brd Nought || isWinFor brd Cross || isDraw brd
